a72b7a022867b7f898fe44bd2e6f6c29474c7711
Leonid_Sanov <sanov.87@mail.ru>
2022-08-07 22:31:26 +0500

    add condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // Параметр ToLower() переводит все символы в нижний регистр
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
