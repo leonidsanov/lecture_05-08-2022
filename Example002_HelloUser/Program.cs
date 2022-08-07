eb0a59f2f25c930d6b6fe46976d130738c2047e8
Leonid_Sanov <sanov.87@mail.ru>
2022-08-07 20:54:11 +0500

    Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Enter your name");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
